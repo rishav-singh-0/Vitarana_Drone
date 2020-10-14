d19e7c273ece587416aa1fb0739bd9c974c2bcaa
Rishav Singh <rsh04613@gmail.com>
2020-10-14 21:28:23 +0530

    some layout changes in task0

--- pkg_task0/script/move.py
+++ pkg_task0/script/move.py
@@ -1,42 +1,48 @@
 #!/usr/bin/env python
 import rospy
 from geometry_msgs.msg import Twist
-PI=3.141592654
+PI = 3.141592654
+
+
 def move():
     rospy.init_node('robot_cleaner', anonymous=True)
-    velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+    velocity_publisher = rospy.Publisher(
+        '/turtle1/cmd_vel', Twist, queue_size=10)
     vel_msg = Twist()
 
     print("Let's move your robot")
-    
-    vel_msg.linear.x =5
-    vel_msg.linear.y =vel_msg.linear.z=vel_msg.angular.x=vel_msg.angular.y=0
+
+    vel_msg.linear.x = 5
+    vel_msg.linear.y = vel_msg.linear.z = vel_msg.angular.x = vel_msg.angular.y = 0
     vel_msg.angular.z = 2
 
-    t0 = rospy.Time.now().to_sec()#it's for initial time which will useful fir calculating the distance
+    # it's for initial time which will useful fir calculating the distance
+    t0 = rospy.Time.now().to_sec()
     cdd = 0
 
-    #let'srevolve the turtle  
-    #it will stop after one revolution  
-    while(cdd <=2*PI*(((vel_msg.angular.z**2)+(vel_msg.linear.x**2))**0.5)/vel_msg.angular.z):
-            #publishing the velocity to the turtle node
+    # let'srevolve the turtle
+    # it will stop after one revolution
+    while(cdd <= 2*PI*(((vel_msg.angular.z**2)+(vel_msg.linear.x**2))**0.5)/vel_msg.angular.z):
+        # publishing the velocity to the turtle node
         velocity_publisher.publish(vel_msg)
-            #taking the second time for claculating the distance
-        t1=rospy.Time.now().to_sec()
-            #calculating distance
-        cdd= vel_msg.linear.x*(t1-t0)
-        rospy.loginfo("moving in circle\n %f",cdd)
-        #rospy.loginfo(cdd)
-        #turtle bot will stop moving after this loop
-    vel_msg.linear.x = 0#makin linear velocity 0 for stopping the bot
+        # taking the second time for claculating the distance
+        t1 = rospy.Time.now().to_sec()
+        # calculating distance
+        cdd = vel_msg.linear.x*(t1-t0)
+        rospy.loginfo("moving in circle\n %f", cdd)
+        # rospy.loginfo(cdd)
+        # turtle bot will stop moving after this loop
+    vel_msg.linear.x = 0  # makin linear velocity 0 for stopping the bot
     rospy.loginfo("goal reached")
-        #it will publish the msg of velocity
+    # it will publish the msg of velocity
     velocity_publisher.publish(vel_msg)
-        #its for user convineance
-    while not rospy.is_shutdown():  
+    # its for user convineance
+    while not rospy.is_shutdown():
         continue
+
 
 if __name__ == '__main__':
     try:
         move()
-    except rospy.ROSInterruptException: pass
+    except rospy.ROSInterruptException:
+        pass
--- pkg_task0/script/revolve_node.py
+++ pkg_task0/script/revolve_node.py
@@ -2,17 +2,18 @@
 import rospy
 from geometry_msgs.msg import Twist
 
+PI = 3.141592654
+
+
 def move():
-    # Starts a new node
     rospy.init_node('robot_cleaner', anonymous=True)
-    velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+    velocity_publisher = rospy.Publisher(
+        '/turtle1/cmd_vel', Twist, queue_size=10)
     vel_msg = Twist()
 
-    #Receiveing the user's input
     print("Let's move your robot")
     speed = 5
 
-    #Since we are moving just in x-axis
     vel_msg.linear.x = speed
     vel_msg.linear.y = 0
     vel_msg.linear.z = 0
@@ -20,30 +21,23 @@ def move():
     vel_msg.angular.y = 0
     vel_msg.angular.z = 2
 
-    while not rospy.is_shutdown():
+    t0 = rospy.Time.now().to_sec()
+    current_distance = 0
 
-        #Setting the current time for distance calculus
-        t0 = rospy.Time.now().to_sec()
-        current_distance = 0
-
-        #Loop to move the turtle in an specified distance
-        distance=  2*3.141592654*(vel_msg.linear.x**2+vel_msg.angular.z**2)**0.5/vel_msg.angular.z
-        while(current_distance < distance):
-            #Publish the velocity
-            velocity_publisher.publish(vel_msg)
-            t1=rospy.Time.now().to_sec()
-            #Calculates distancePoseStamped
-            current_distance= speed*(t1-t0)
-            print(current_distance)
-            #if (current_distance > distance): break
-        #After the loop, stops the robot
-        vel_msg.linear.x = 0
-        #Force the robot to stop
+    def distance(v, w): return 2*PI * (v**2 + w**2)**0.5/w
+    while(current_distance < distance(vel_msg.linear.x, vel_msg.angular.z)):
         velocity_publisher.publish(vel_msg)
+        t1 = rospy.Time.now().to_sec()
+        current_distance = speed*(t1-t0)
+        rospy.loginfo("Moving in Circle")
+        print(current_distance)
+    velocity_publisher.publish(vel_msg)
+    while not rospy.is_shutdown():
+        continue
+
 
 if __name__ == '__main__':
     try:
-        #Testing our function
         move()
-    except rospy.ROSInterruptException: pass
-
+    except rospy.ROSInterruptException:
+        pass
