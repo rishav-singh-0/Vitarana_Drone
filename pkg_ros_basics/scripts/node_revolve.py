a8cc119649aa554ea46a2d21f5a10888f731fdce
379-kash <kashyap379@gmail.com>
2020-10-18 08:20:28 +0530

    uploding node_rovolve.py

--- pkg_ros_basics/scripts/node_revolve.py
+++ pkg_ros_basics/scripts/node_revolve.py
@@ -10,6 +10,8 @@ e=0
 RA=2.18*PI#calculating required angle in radian
 expected_theta=0.08481469750404358#its expected theeta figured out from rostopic "echo /turtle1/pose"  
 #this is call back function for subcribing 'turtle1/pose'
+f=0.09
+radius=1
 def printer(Pose):
     global y
     y=Pose.theta
@@ -25,24 +27,24 @@ def move():
         rospy.Subscriber('turtle1/pose',Pose,printer)#Subcribing the 'turtle1/pose' topic
         vel_msg = Twist()
         #assigning the velocities
-        vel_msg.linear.x = 2*2*PI*0.2
+        vel_msg.linear.x = radius*2*PI*f
         vel_msg.linear.y = vel_msg.linear.z = vel_msg.angular.x = vel_msg.angular.y = 0
-        vel_msg.angular.z =2*PI*0.2
+        vel_msg.angular.z =2*PI*f
         velocity_publisher.publish(vel_msg)#publishing the velocities
 
         t1 = rospy.Time.now().to_sec()#current time
         current_distance = speed*(t1-t0)#calculation of distance
 
-        #rospy.loginfo("Moving in Circle\n%f",(vel_msg.angular.z*(t1-t0)))#printing the radian measurement
+        rospy.loginfo("Moving in Circle\n%f",(vel_msg.angular.z*(t1-t0)))#printing the radian measurement
         #below block is for stopping the turtle
-        #print(y)
+        print(y)
         global e
         def distance(v, w): return 2*PI * (v**2 + w**2)**0.5/w
         if(e==0):
             while(round(y,1)==0.0):
-                vel_msg.linear.x = 2*2*PI*0.2
+                vel_msg.linear.x = 2*2*PI*f
                 vel_msg.linear.y = vel_msg.linear.z = vel_msg.angular.x = vel_msg.angular.y = 0
-                vel_msg.angular.z =2*PI*0.2
+                vel_msg.angular.z =2*PI*f
                 velocity_publisher.publish(vel_msg)
                 #print("1")
                 continue
@@ -50,10 +52,10 @@ def move():
             # if(e<3):
             #     e=e+1
             #     print(e)
-        elif((e==1)and(round(y,1)!=0.1)):
-            vel_msg.linear.x = 2*2*PI*0.2
-            vel_msg.linear.y = vel_msg.linear.z = vel_msg.angular.x = vel_msg.angular.y = 0
-            vel_msg.angular.z =2*PI*0.2
+        elif((e==1)and(round(y,1)!=0.0)):
+            # vel_msg.linear.x = 2*2*PI*f
+            # vel_msg.linear.y = vel_msg.linear.z = vel_msg.angular.x = vel_msg.angular.y = 0
+            # vel_msg.angular.z =2*PI*f
             velocity_publisher.publish(vel_msg)
         else:
 
@@ -61,7 +63,7 @@ def move():
             vel_msg.angular.z = 0
             velocity_publisher.publish(vel_msg)
             break
-        rospy.sleep(0.2)
+        rospy.sleep(f)
     #rospy.loginfo("goal reached")
     #waiting for user to terminate the program
     while not rospy.is_shutdown():
