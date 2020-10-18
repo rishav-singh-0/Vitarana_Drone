69b2689cdaed1f54b9797758deb02e8a55e36fdf
Rishav Singh <rsh04613@gmail.com>
2020-10-18 18:32:35 +0530

    Task 0 finally finally completeds

--- pkg_task0/script/final.py
+++ pkg_task0/script/final.py
@@ -3,16 +3,18 @@ import rospy
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 
-PI = 3.141592
+counter = 0
+counter1 = 0
+x, x0, y, y0 = 0, 0, 0, 0
+error = 0.01
 
 
 def initialCall(Pose):
-    x0, y0 = Pose.x, Pose.y
-    print(x0, y0)
-
-
-# def check():
-#     if()
+    global x, y, x0, y0, counter
+    if counter == 0:
+        counter = 1
+        x0, y0 = Pose.x, Pose.y
+    x, y = Pose.x, Pose.y
 
 
 def main():
@@ -20,19 +22,40 @@ def main():
     v_pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
     v_msg = Twist()
 
-    print("Lets move out Bot")
+    t0 = rospy.Time.now().to_sec()
+    print("Lets move our Bot")
 
-    v_msg.linear.x = 5
+    v_msg.linear.x = 2
     v_msg.linear.y = 0
     v_msg.linear.z = 0
     v_msg.angular.x = 0
     v_msg.angular.y = 0
-    v_msg.angular.z = 2
-
-    rospy.Subscriber('turtle1/pose', Pose, initialCall)
+    v_msg.angular.z = 1
     v_pub.publish(v_msg)
-    # while(check):
+    rospy.Subscriber('turtle1/pose', Pose, initialCall)
 
+    global counter1
+    while(True):
+        rospy.Rate(15).sleep()
+        t1 = rospy.Time.now().to_sec()
+        v_pub.publish(v_msg)
+        rad = v_msg.angular.z * (t1-t0)
+        rospy.loginfo("Moving in Circle\n%f", rad)
+
+        global counter1
+        if(counter1 == 0):
+            while int(y) == int(y0):
+                v_pub.publish(v_msg)
+                continue
+            counter1 = counter1+1
+            continue
+
+        if(y <= y0+error):
+            v_msg.linear.x = 0
+            v_pub.publish(v_msg)
+            break
+
+    rospy.loginfo("Goal Reached")
     while not rospy.is_shutdown():
         v_pub.publish(v_msg)
 
--- pkg_task0/script/semifinal.py
+++ pkg_task0/script/semifinal.py
@@ -2,93 +2,58 @@
 import rospy
 from turtlesim.msg import Pose
 from geometry_msgs.msg import Twist
-import time 
-y=0#it is defined as global variable to take the theta from the 'turtle1/pose' topic
-speed=5#it is linear velocity of the x 
-PI = 3.141592654
-e=0
-q=0
-RA=2.18*PI#calculating required angle in radian
-##its expected theeta figured out from rostopic "echo /turtle1/pose"  
-#this is call back function for subcribing 'turtle1/pose'
-f=0.2
-radius=1
-x=0
-varx=0
-vary=0
+import time
+e = 0
+q = 0
+
+
 def printer(Pose):
-    global y
-    global x
-    global e
-    global varx
-    global vary
-    while (Pose.x !=0 and Pose.y !=0 and e==0):
-        varx=Pose.x
-        vary=Pose.y
-        e=e+1
-    y=Pose.y
-    x=Pose.x
-    #print(x,"and",y)
-#r =rospy.Rate(10)    
-#its main working function for moving the turtle bot
+    global y, x, e, varx, vary
+    while (Pose.x != 0 and Pose.y != 0 and e == 0):
+        varx = Pose.x
+        vary = Pose.y
+        e = e+1
+    y = Pose.y
+    x = Pose.x
+
+
 def move():
-    rospy.Subscriber('turtle1/pose',Pose,printer)#Subcribing the 'turtle1/pose' topic
-    #print(x,"and",y)
-    rospy.init_node('mover',anonymous=True)#making the node for communication
+    rospy.Subscriber('turtle1/pose', Pose, printer)
+    rospy.init_node('mover', anonymous=True)
     velocity_publisher = rospy.Publisher(
-            '/turtle1/cmd_vel', Twist, queue_size=10)#its a publishing functios which will cofigure the '/turtle1/cmd_vel' and also queue size
-    t0 = rospy.Time.now().to_sec()#inital time
-    current_distance = 0#inial measured distance
+        '/turtle1/cmd_vel', Twist, queue_size=10)  # its a publishing functios which will cofigure the '/turtle1/cmd_vel' and also queue size
+    t0 = rospy.Time.now().to_sec()  # inital time
     while (True):
         vel_msg = Twist()
-        #assigning the velocities
         vel_msg.linear.x = 5
         vel_msg.linear.y = vel_msg.linear.z = vel_msg.angular.x = vel_msg.angular.y = 0
-        vel_msg.angular.z =2
-        velocity_publisher.publish(vel_msg)#publishing the velocities
-        rospy.Subscriber('turtle1/pose',Pose,printer)#Subcribing the 'turtle1/pose' topic
+        vel_msg.angular.z = 2
+        velocity_publisher.publish(vel_msg)  # publishing the velocities
+        rospy.Subscriber('turtle1/pose', Pose, printer)
 
-        t1 = rospy.Time.now().to_sec()#current time
-        current_distance = speed*(t1-t0)#calculation of distance
+        t1 = rospy.Time.now().to_sec()  # current time
 
-        #rospy.loginfo("Moving in Circle\n%f",(vel_msg.angular.z*(t1-t0)))#printing the radian measurement
-        #below block is for stopping the turtle
-       # print(varx)
-
-        
         global q
-        def distance(v, w): return 2*PI * (v**2 + w**2)**0.5/w
-        print(x,"and",y)
-        if(q==0):
-            while(((round(y,0)==round(vary,0)))):
-                vel_msg.linear.x = 5
-                vel_msg.linear.y = vel_msg.linear.z = vel_msg.angular.x = vel_msg.angular.y = 0
-                vel_msg.angular.z =2
+        if(q == 0):
+            while int(y) == int(vary):
                 velocity_publisher.publish(vel_msg)
                 continue
-            q=q+1
+            q = q+1
+            continue
 
-        elif((q==1)and(round(y,1)==round(vary,1))):
-            # velocity_publisher.publish(vel_msg)
-            print(x,"and",y)
-            # print("you are great")
+        if (round(y, 1) == round(vary, 1)):
             vel_msg.linear.x = 0
-            vel_msg.angular.z = 2
             velocity_publisher.publish(vel_msg)
             break
 
-        # else:
-        #     vel_msg.linear.x = 0
-        #     vel_msg.angular.z = 2
-        #     velocity_publisher.publish(vel_msg)
-        #     break
-        #rospy.sleep(f)
     rospy.loginfo("goal reached")
-    #waiting for user to terminate the program
+    # waiting for user to terminate the program
     while not rospy.is_shutdown():
-        vel_msg.angular.z =2
+        vel_msg.angular.z = 2
         velocity_publisher.publish(vel_msg)
         continue
+
+
 if __name__ == '__main__':
     try:
         move()
