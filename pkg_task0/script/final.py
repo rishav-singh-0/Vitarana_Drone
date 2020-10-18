cf624b6a5be82349eec440b64ec462b7479c0ad6
Rishav Singh <rsh04613@gmail.com>
2020-10-18 12:00:03 +0530

    final revolution node

--- pkg_task0/script/final.py
+++ pkg_task0/script/final.py
@@ -1,55 +1,44 @@
 #!/usr/bin/env python
 import rospy
-from turtlesim.msg import Pose
 from geometry_msgs.msg import Twist
-j = 0
-k = 0
-y = 0
-t = 0
-PI = 3.141592654
+from turtlesim.msg import Pose
+
+PI = 3.141592
 
 
-def printer(Pose):
-    global j
-    global k
-    global y
-    global t
-    j = Pose.x
-    t = Pose.x
-    y = Pose.theta
-    # y=Pose.linear_velocity
-    # print(Pose.x)
+def initialCall(Pose):
+    x0, y0 = Pose.x, Pose.y
+    print(x0, y0)
 
 
-def move():
+# def check():
+#     if()
 
-    e = 0
-    while (True):
-        rospy.Subscriber('turtle1/pose', Pose, printer)
-        rospy.init_node('mover', anonymous=True)
-        velocity_publisher = rospy.Publisher(
-            '/turtle1/cmd_vel', Twist, queue_size=10)
-        # print(j,"and",k)
-        # print(k,"and",y)
-        vel_msg = Twist()
-        vel_msg.linear.x = 5
-        vel_msg.linear.y = vel_msg.linear.z = vel_msg.angular.x = vel_msg.angular.y = 0
-        vel_msg.angular.z = 2
-        velocity_publisher.publish(vel_msg)
 
-        if(y == 0.08481469750404358):
-            # if(e==0):
-            #     e=e+1
-            #     print(e)
-            # else:
-            vel_msg.linear.x = 0
-            vel_msg.angular.z = 2
-            velocity_publisher.publish(vel_msg)
-            break
+def main():
+    rospy.init_node("revolve", anonymous=True)
+    v_pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+    v_msg = Twist()
+
+    print("Lets move out Bot")
+
+    v_msg.linear.x = 5
+    v_msg.linear.y = 0
+    v_msg.linear.z = 0
+    v_msg.angular.x = 0
+    v_msg.angular.y = 0
+    v_msg.angular.z = 2
+
+    rospy.Subscriber('turtle1/pose', Pose, initialCall)
+    v_pub.publish(v_msg)
+    # while(check):
+
+    while not rospy.is_shutdown():
+        v_pub.publish(v_msg)
 
 
 if __name__ == '__main__':
     try:
-        move()
+        main()
     except rospy.ROSInterruptException:
         pass
