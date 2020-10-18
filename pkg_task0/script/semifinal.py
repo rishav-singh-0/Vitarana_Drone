978f1ad15b46e7224bf5a76bbeb6e56d0efacc12
379-kash <kashyap379@gmail.com>
2020-10-18 10:34:28 +0530

    Minor Changes

--- pkg_task0/script/semifinal.py
+++ pkg_task0/script/semifinal.py
@@ -7,58 +7,86 @@ y=0#it is defined as global variable to take the theta from the 'turtle1/pose' t
 speed=5#it is linear velocity of the x 
 PI = 3.141592654
 e=0
+q=0
 RA=2.18*PI#calculating required angle in radian
-expected_theta=0.08481469750404358#its expected theeta figured out from rostopic "echo /turtle1/pose"  
+##its expected theeta figured out from rostopic "echo /turtle1/pose"  
 #this is call back function for subcribing 'turtle1/pose'
-f=0.4
+f=0.2
 radius=1
+x=0
+varx=0
+vary=0
 def printer(Pose):
     global y
-    y=Pose.theta
+    global x
+    global e
+    global varx
+    global vary
+    while (Pose.x !=0 and Pose.y !=0 and e==0):
+        varx=Pose.x
+        vary=Pose.y
+        e=e+1
+    y=Pose.y
+    x=Pose.x
+    #print(x,"and",y)
 #r =rospy.Rate(10)    
 #its main working function for moving the turtle bot
 def move():
+    rospy.Subscriber('turtle1/pose',Pose,printer)#Subcribing the 'turtle1/pose' topic
+    #print(x,"and",y)
     rospy.init_node('mover',anonymous=True)#making the node for communication
     velocity_publisher = rospy.Publisher(
             '/turtle1/cmd_vel', Twist, queue_size=10)#its a publishing functios which will cofigure the '/turtle1/cmd_vel' and also queue size
     t0 = rospy.Time.now().to_sec()#inital time
     current_distance = 0#inial measured distance
     while (True):
-        rospy.Subscriber('turtle1/pose',Pose,printer)#Subcribing the 'turtle1/pose' topic
         vel_msg = Twist()
         #assigning the velocities
-        vel_msg.linear.x = radius*2*PI*f
+        vel_msg.linear.x = 5
         vel_msg.linear.y = vel_msg.linear.z = vel_msg.angular.x = vel_msg.angular.y = 0
-        vel_msg.angular.z =2*PI*f
+        vel_msg.angular.z =2
         velocity_publisher.publish(vel_msg)#publishing the velocities
+        rospy.Subscriber('turtle1/pose',Pose,printer)#Subcribing the 'turtle1/pose' topic
 
         t1 = rospy.Time.now().to_sec()#current time
         current_distance = speed*(t1-t0)#calculation of distance
 
-        rospy.loginfo("Moving in Circle\n%f",(vel_msg.angular.z*(t1-t0)))#printing the radian measurement
+        #rospy.loginfo("Moving in Circle\n%f",(vel_msg.angular.z*(t1-t0)))#printing the radian measurement
         #below block is for stopping the turtle
-        global e
+       # print(varx)
+
+        
+        global q
         def distance(v, w): return 2*PI * (v**2 + w**2)**0.5/w
-        if(e==0):
-            while(round(y,1)==0.0):
-                vel_msg.linear.x = 2*PI*f
+        print(x,"and",y)
+        if(q==0):
+            while(((round(y,0)==round(vary,0)))):
+                vel_msg.linear.x = 5
                 vel_msg.linear.y = vel_msg.linear.z = vel_msg.angular.x = vel_msg.angular.y = 0
-                vel_msg.angular.z =2*PI*f
+                vel_msg.angular.z =2
                 velocity_publisher.publish(vel_msg)
                 continue
-            e=e+1
-        elif((e==1)and(round(y,0)!=0)):
-            velocity_publisher.publish(vel_msg)
-        else:
+            q=q+1
+
+        elif((q==1)and(round(y,1)==round(vary,1))):
+            # velocity_publisher.publish(vel_msg)
+            print(x,"and",y)
+            # print("you are great")
             vel_msg.linear.x = 0
-            vel_msg.angular.z = 2*radius*PI*f
+            vel_msg.angular.z = 2
             velocity_publisher.publish(vel_msg)
             break
-        rospy.sleep(f)
+
+        # else:
+        #     vel_msg.linear.x = 0
+        #     vel_msg.angular.z = 2
+        #     velocity_publisher.publish(vel_msg)
+        #     break
+        #rospy.sleep(f)
     rospy.loginfo("goal reached")
     #waiting for user to terminate the program
     while not rospy.is_shutdown():
-        vel_msg.angular.z =2*PI*0.2
+        vel_msg.angular.z =2
         velocity_publisher.publish(vel_msg)
         continue
 if __name__ == '__main__':
