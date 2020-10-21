db5f17e234eadd37a01140cad0eda6be6e488b63
Rishav Singh <rsh04613@gmail.com>
2020-10-21 11:54:22 +0530

    Task1 init

--- pkg_task1/script/1_Controller.py
+++ pkg_task1/script/1_Controller.py
@@ -1,1 +1,18 @@
+#!/usr/bin/env python
+
+# importing required libraries
 import rospy
+from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
+
+
+def main():
+    # initialising node
+    rospy.init_node("1_controller", anonymous=True)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
