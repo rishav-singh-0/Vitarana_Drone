8adadd9aec6d43e0451824078503f2338102220a
379-kash <kashyap379@gmail.com>
2020-10-22 10:17:18 +0530

    sample program for the altitude control

--- pkg_task1/script/sample_control.py
+++ pkg_task1/script/sample_control.py
@@ -2,40 +2,41 @@
 import rospy
 from sensor_msgs.msg import NavSatFix
 from vitarana_drone.msg import prop_speed
-a,a0,cnt=0,0,0
+current_altitude,a0,cnt=0,0,0
 def frange(NavSatFix):
-    global a,a0,cnt
+    global current_altitude,a0,cnt
     if cnt == 0:
         cnt = 1
         a0=NavSatFix.altitude
-    a=NavSatFix.altitude
+    current_altitude=NavSatFix.altitude
     
-x=512.0
-c=0
+pwm=512.0
+desired_altitude=2
+ep=ed=ei=ec=input_ctrl_signal=t0=t1=0
+kp=0.01
+kd=0.2
+ki=0.5
 def stable():
-    global x
-    global c
+    global pwm
+    global desired_altitude,ep,ed,ei,ec,input_ctrl_signal,kp,kd,ki,t0,t1
     rospy.init_node("hover",anonymous=True)
     p_pub = rospy.Publisher('/edrone/pwm',prop_speed,queue_size=1)
     p_msg=prop_speed()
+    t0 = rospy.Time.now().to_sec()
     rospy.Subscriber('/edrone/gps',NavSatFix,frange)
-
-    while not rospy.is_shutdown():
-        if(a>1):
-            x-=0.05
-            c=1
-        while(c==1 and a<1):
-            x+=0.05
-            p_msg.prop1=p_msg.prop2=p_msg.prop3=p_msg.prop4=x
-            p_pub.publish(p_msg)
-            if(a==1):
-                break
-
-        
-
-        p_msg.prop1=p_msg.prop2=p_msg.prop3=p_msg.prop4=x
+    ep=current_altitude
+    ei=0
+    while(True):
+        ec=desired_altitude-ep
+        t1 = rospy.Time.now().to_sec()
+        ed=(ep-ec)
+        ei=ei+ec*(t1-t0)
+        ep=ec
+        input_ctrl_signal= kp*ec + kd*ed + ki*ei
+        print(input_ctrl_signal,"and altitude is",current_altitude)
+        p_msg.prop1=p_msg.prop2=p_msg.prop3=p_msg.prop4=pwm
         p_pub.publish(p_msg)
-        print(a)
+        #print()
 
 if __name__ == '__main__':
     try:
