de04fdaecf43b83ca08b703d190c78af5829979f
379-kash <kashyap379@gmail.com>
2020-11-02 17:01:06 +0530

    major comments were added

--- pkg_task1/script/position_controller.py
+++ pkg_task1/script/position_controller.py
@@ -8,9 +8,12 @@ from pid_tune.msg import PidTune
 from std_msgs.msg import Float32
 from sensor_msgs.msg import NavSatFix
 
+# class for position_controller.py
+
 
 class Command():
     '''Navigating the Drone through commands'''
+    # constructor
 
     def __init__(self):
         rospy.init_node('position_controller')
@@ -31,16 +34,17 @@ class Command():
         self.Kp = [630*100,  630*100,  290*1]
         self.Ki = [15*0.008, 15*0.008,  0*0.25]
         self.Kd = [1503*40,  1503*40,   284*1]
-
+        # necessary variables for calculation of desired position for roll,pitch and throttle
         self.error = [0, 0, 0]
         self.prev_error = [0, 0, 0]
         self.change = [0, 0, 0]
         self.sum = [0, 0, 0]
         self.output = [0, 0, 0]
 
-        self.sample_time = 0.010
+        self.sample_time = 0.010  # sample time
+        # equilibrium point for all the angle(roll,pitch,yaw) and throttle
         self.equilibrium_value = 1500
-
+        # object for publishing the rc messages
         self.setpoint_cmd = edrone_cmd()
 
         # Publishing /pitch_error, /roll_error, /throttle_error
@@ -60,21 +64,21 @@ class Command():
     def gps_callback(self, msg):
         self.gps_position = [msg.latitude, msg.longitude, msg.altitude]
 
-    def roll_set_pid(self, roll):
-        self.Kp[0] = roll.Kp * 100
-        self.Ki[0] = roll.Ki * 0.008
-        self.Kd[0] = roll.Kd * 40
+    # def roll_set_pid(self, roll):
+    #     self.Kp[0] = roll.Kp * 100
+    #     self.Ki[0] = roll.Ki * 0.008
+    #     self.Kd[0] = roll.Kd * 40
 
-    def pitch_set_pid(self, pitch):
-        self.Kp[1] = pitch.Kp * 100
-        self.Ki[1] = pitch.Ki * 0.008
-        self.Kd[1] = pitch.Kd * 0.3
+    # def pitch_set_pid(self, pitch):
+    #     self.Kp[1] = pitch.Kp * 100
+    #     self.Ki[1] = pitch.Ki * 0.008
+    #     self.Kd[1] = pitch.Kd * 0.3
 
-    def throttle_set_pid(self, throttle):
-        self.Kp[2] = throttle.Kp * 1
-        self.Ki[2] = throttle.Ki * 0.25
-        self.Kd[2] = throttle.Kd * 1
-
+    # def throttle_set_pid(self, throttle):
+    #     self.Kp[2] = throttle.Kp * 1
+    #     self.Ki[2] = throttle.Ki * 0.25
+    #     self.Kd[2] = throttle.Kd * 1
+    # this function will convert all rc messages in the range of 1000 to 2000
     def check(self, operator):
         if operator > 2000:
             return 2000
@@ -82,6 +86,7 @@ class Command():
             return 1000
         else:
             return operator
+    # function will hendle all desired positions
 
     def destination_check(self):
 
@@ -94,6 +99,7 @@ class Command():
                 if -0.2 < self.error[2] < 0.2:
                     self.next_destination += 1
                     print("destination reached")
+    # function for implimenting the pid algorithm
 
     def pid(self):
 
@@ -106,17 +112,15 @@ class Command():
             self.sum[i] = self.sum[i] + self.error[i] * self.sample_time
             self.output[i] = self.Kp[i] * self.error[i] + \
                 self.Kd[i]*self.change[i] + self.Ki[i]*self.sum[i]
-
+        # figure out the values  for roll,pitch and throttle
         self.setpoint_cmd.rcRoll = self.check(
             self.equilibrium_value + 100*self.output[0])
-        # self.setpoint_cmd.rcRoll = self.equilibrium_value
         self.setpoint_cmd.rcPitch = self.check(
             self.equilibrium_value + 100*self.output[1])
-        # self.setpoint_cmd.rcPitch = self.equilibrium_value
         self.setpoint_cmd.rcThrottle = self.check(
             self.equilibrium_value + self.output[2])
         self.setpoint_cmd.rcYaw = self.equilibrium_value
-
+        # publishing all the values to attitude_controller and for plotting purpose
         self.roll_pub.publish(self.error[0])
         self.pitch_pub.publish(self.error[1])
         self.throttle_pub.publish(self.error[2])
@@ -127,8 +131,8 @@ if __name__ == '__main__':
 
     # specify rate in Hz based upon your desired PID sampling time
     command = Command()
-    rate = rospy.Rate(1/command.sample_time)
+    rate = rospy.Rate(1/command.sample_time)  # defining rate
     while not rospy.is_shutdown():
         command.pid()
         command.destination_check()
-        rate.sleep()
+        rate.sleep()  # frequency of 100 Hz
