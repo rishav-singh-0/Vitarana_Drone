967f7a9c25186ab11220fa79b9a65a2382971a30
Rishav Singh <rsh04613@gmail.com>
2020-11-07 17:27:26 +0530

    Task 2 work started

--- pkg_task2/scripts/qr_detect.py
+++ pkg_task2/scripts/qr_detect.py
@@ -1,36 +1,40 @@
 #!/usr/bin/env python
 
-
-'''
-This is a boiler plate script that contains an example on how to subscribe a rostopic containing camera frames 
-and store it into an OpenCV image to use it further for image processing tasks.
-Use this code snippet in your code or you can also continue adding your code in the same file
-'''
-
-
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
+from pyzbar.pyzbar import decode
 import numpy as np
 import rospy
 
+
 class image_proc():
 
-	# Initialise everything
-	def __init__(self):
-		rospy.init_node('barcode_test') #Initialise rosnode 
-		self.image_sub = rospy.Subscriber("/edrone/camera/image_raw", Image, self.image_callback) #Subscribing to the camera topic
-		self.img = np.empty([]) # This will contain your image frame from camera
-		self.bridge = CvBridge()
+    # Initialise everything
+    def __init__(self):
+        rospy.init_node('barcode_scan')  # Initialise rosnode
+        # Subscribing to the camera topic
+        self.image_sub = rospy.Subscriber(
+            "/edrone/camera/image_raw", Image, self.image_callback)
+        # This will contain your image frame from camera
+        self.img = np.empty([])
+        self.bridge = CvBridge()
 
+    # Callback function of camera topic
 
-	# Callback function of amera topic
-	def image_callback(self, data):
-		try:
-			self.img = self.bridge.imgmsg_to_cv2(data, "bgr8") # Converting the image to OpenCV standard image
-		except CvBridgeError as e:
-			print(e)
-			return
+    def image_callback(self, data):
+        try:
+            # Converting the image to OpenCV standard image
+            self.img = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+            return
+
+    def read_qr(self):
+        barcode = decode(self.img)
+        for code in barcode:
+            print(code.data.decode('utf-8'))
+
 
 if __name__ == '__main__':
     image_proc_obj = image_proc()
