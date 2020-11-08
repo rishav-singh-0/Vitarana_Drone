1436a3d3d4fc28b89366c6c34244a8b803d59200
Rishav Singh <rsh04613@gmail.com>
2020-11-08 11:55:49 +0530

    Task 2 work started 1

--- pkg_task2/scripts/qr_detect.py
+++ pkg_task2/scripts/qr_detect.py
@@ -13,11 +13,13 @@ class image_proc():
     # Initialise everything
     def __init__(self):
         rospy.init_node('barcode_scan')  # Initialise rosnode
+
+        # This will contain your image frame from camera
+        self.img = np.empty([])
+
         # Subscribing to the camera topic
         self.image_sub = rospy.Subscriber(
             "/edrone/camera/image_raw", Image, self.image_callback)
-        # This will contain your image frame from camera
-        self.img = np.empty([])
         self.bridge = CvBridge()
 
     # Callback function of camera topic
@@ -26,16 +28,29 @@ class image_proc():
         try:
             # Converting the image to OpenCV standard image
             self.img = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            cv2.imshow("Image window", self.img)
         except CvBridgeError as e:
             print(e)
             return
 
     def read_qr(self):
-        barcode = decode(self.img)
-        for code in barcode:
-            print(code.data.decode('utf-8'))
+        print(type(self.img), int(self.img))
+        # barcode = decode(self.img)
+        print(self.img)
+        # # (rows, cols, channels) = self.img.shape
+        # if cols > 60 and rows > 60:
+        #     cv2.circle(self.img, (50, 50), 10, 255)
+
+        cv2.imshow("Image window", self.img)
+        # cv2.imshow(self.img)
+        # for code in barcode:
+        #     print(code.data.decode('utf-8'))
 
 
 if __name__ == '__main__':
     image_proc_obj = image_proc()
+    r = rospy.Rate(1/1)
+    while not rospy.is_shutdown():
+        image_proc_obj.read_qr()
+        r.sleep()
     rospy.spin()
