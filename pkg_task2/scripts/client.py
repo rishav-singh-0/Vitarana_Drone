598c5651939d18f76af16c048b8bd37e106804ed
379-kash <kashyap379@gmail.com>
2020-11-09 17:33:38 +0530

    Detech Almost Done

---
+++ pkg_task2/scripts/client.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+from vitarana_drone.srv import *
+from vitarana_drone.msg import *
+import rospy
+from std_msgs.msg import String
+from std_msgs.msg import Float32
+i = None
+j = None
+
+
+def call_back(S):
+    global i
+    i = S.data
+    print(i)
+
+
+def client(x):
+    rospy.wait_for_service('/edrone/activate_gripper')
+    pover = rospy.ServiceProxy('/edrone/activate_gripper', Gripper)
+    reasult = pover(x)
+    return reasult.result
+
+
+def detech_msg(msg):
+    global j
+    j = msg.data
+
+
+w = 0
+
+
+def main():
+    global w
+    rospy.init_node('pose')
+    rospy.Subscriber('/edrone/gripper_check', String, call_back)
+    rospy.Subscriber('op_flag', Float32, detech_msg)
+    # print(i)
+    if(i == 'True'):
+        print(client(bool(1)))
+    elif(j == 1):
+        print(client(bool(0)))
+    elif(w == 0):
+        print(client(bool(0)))
+        w += 1
+
+    # x = i
+    # print(client(i))
+if __name__ == "__main__":
+    while not rospy.is_shutdown():
+        main()
+
+    # x = bool(0)
+    # print(client(x))
