287f0e2fd7c712f5521dd7e4fb08c0eae366055f
379-kash <kashyap379@gmail.com>
2020-11-09 20:42:51 +0530

    further bugs are fixed

--- pkg_task2/scripts/client.py
+++ pkg_task2/scripts/client.py
@@ -4,14 +4,12 @@ from vitarana_drone.msg import *
 import rospy
 from std_msgs.msg import String
 from std_msgs.msg import Float32
-i = None
-j = None
+attech_situation, detech_req = None, None
 
 
 def call_back(S):
-    global i
-    i = S.data
-    # print(i)
+    global attech_situation
+    attech_situation = S.data
 
 
 def client(x):
@@ -22,33 +20,22 @@ def client(x):
 
 
 def detech_msg(msg):
-    global j
-    j = msg.data
-    print(j)
-
-
-w = 0
+    global detech_req
+    detech_req = msg.data
+    print(detech_req)
 
 
 def main():
-    global w
     rospy.init_node('pose')
     rospy.Subscriber('/edrone/gripper_check', String, call_back)
     rospy.Subscriber('op_flag', Float32, detech_msg)
     # print(i)
-    if(i == 'True' and j == 0):
+    if(attech_situation == 'True' and detech_req == 0):
         client(bool(1))
-    elif(j == 1 and i == 'True'):
+    elif(detech_req >= 1 and attech_situation == 'True'):
         client(bool(0))
-    # elif(w == 0):
-    #     client(bool(0))
-    #     w += 1
 
-    # x = i
-    # print(client(i))
+
 if __name__ == "__main__":
     while not rospy.is_shutdown():
         main()
-
-    # x = bool(0)
-    # print(client(x))
