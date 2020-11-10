b74c70d9ab4bdd3437d030466186b57cd4178117
Rishav Singh <rsh04613@gmail.com>
2020-11-11 03:15:05 +0530

    worked on path planning algo

--- pkg_task2/scripts/path_planner.py
+++ pkg_task2/scripts/path_planner.py
@@ -1,2 +1,42 @@
 import rospy
 from sensor_msgs.msg import NavSatFix
+
+
+class PathPlanner():
+
+    def __init__(self):
+        rospy.init_node('/path_planner')
+
+        # Destination to be reached
+        # [latitude, longitude, altitude]
+        self.destination = [0, 0, 0]
+
+        # Present Location of the Drone
+        self.current_location = [0, 0, 0]
+
+        self.checkpoint = NavSatFix()
+
+        # Publisher
+        self.pub_checkpoint = rospy.Publisher('/checkpoint', NavSatFix, queue_size=1)
+
+        # Subscriber
+        rospy.Subscriber('/final_setpoint', NavSatFix, final_setpoint_callback)
+        rospy.Subscriber('/edrone/gps', NavSatFix, gps_callback)
+
+    def final_setpoint_callback(self, msg):
+        self.destination = [msg.latitude, msg.longitude, msg.altitude]
+
+    def gps_callback(self, msg):
+        self.current_location = [msg.latitude, msg.longitude, msg.altitude]
+
+    def planner(self):
+
+        ##############################################################
+        # Main Algorithm
+        ##############################################################
+
+        self.checkpoint.latitude =0
+        self.checkpoint.longitude =0
+        self.checkpoint.altitude =0
+
+        self.pub_checkpoint.publish(self.checkpoint)
