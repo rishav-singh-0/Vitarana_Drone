0c64a5966ac3fbf14768934e84e0c6971d886fb5
Rishav Singh <rsh04613@gmail.com>
2020-11-11 21:59:16 +0530

    Gripper: Changed to Class component

--- pkg_task2/scripts/client.py
+++ pkg_task2/scripts/client.py
@@ -7,56 +7,42 @@ from std_msgs.msg import Float32
 attech_situation, detech_req = None, None
 
 
-def call_back(S):
-    global attech_situation
-    attech_situation = S.data
+class Grip():
+    def __init__(self):
+        rospy.init_node('pose')
+
+        self.attech_situation = False
+        self.detech_msg = False
+        self.detech_req = False
+
+        rospy.Subscriber('/edrone/gripper_check', String, self.call_back)
+        rospy.Subscriber('op_flag', Float32, self.detech_msg)
+
+    def call_back(self, state):
+        self.attech_situation = state.data
 
 
-def client(x):
-    rospy.wait_for_service('/edrone/activate_gripper')
-    pover = rospy.ServiceProxy('/edrone/activate_gripper', Gripper)
-    reasult = pover(x)
-    return reasult.result
+    def client(self, x):
+        rospy.wait_for_service('/edrone/activate_gripper')
+        pover = rospy.ServiceProxy('/edrone/activate_gripper', Gripper)
+        reasult = pover(x)
+        return reasult.result
 
 
-def detech_msg(msg):
+    def detech_msg(self, msg):
+        self.detech_req = msg.data
+        # print(self.detech_req)
 
 
-<< << << < HEAD
-   global j
-    j = msg.data
-
-
-w = 0
-== == == =
-   global detech_req
-    detech_req = msg.data
-    print(detech_req)
->>>>>> > 1bd2250... further bugs are fixed
-
-
-def main():
-    rospy.init_node('pose')
-    rospy.Subscriber('/edrone/gripper_check', String, call_back)
-    rospy.Subscriber('op_flag', Float32, detech_msg)
-
-    # print(i)
-<< << << < HEAD
-   if(i == 'True'):
-        print(client(bool(1)))
-    elif(j == 1):
-        print(client(bool(0)))
-    elif(w == 0):
-        print(client(bool(0)))
-        w += 1
-== =====
-   if(attech_situation == 'True' and detech_req == 0):
-        client(bool(1))
-    elif(detech_req >= 1 and attech_situation == 'True'):
-        client(bool(0))
->>>>>> > 1bd2250... further bugs are fixed
+    def main(self):
+        # print(i)
+        if(self.attech_situation == 'True' and self.detech_req == 0):
+            self.client(bool(1))
+        elif(self.detech_req >= 1 and self.attech_situation == 'True'):
+            self.client(bool(0))
 
 
 if __name__ == "__main__":
+    grip = Grip()
     while not rospy.is_shutdown():
-        main()
+        grip.main()
