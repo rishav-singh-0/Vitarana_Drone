d02c97c4295b56a28c714596ae85e3c8c7b4eeb9
Rishav Singh <rsh04613@gmail.com>
2020-11-12 17:51:01 +0530

    Minor Changes

--- pkg_task2/scripts/path_planner.py
+++ pkg_task2/scripts/path_planner.py
@@ -1,5 +1,5 @@
 import rospy
-from sensor_msgs.msg import NavSatFix
+from sensor_msgs.msg import NavSatFix, LaserScan
 
 
 class PathPlanner():
@@ -14,7 +14,12 @@ class PathPlanner():
         # Present Location of the Drone
         self.current_location = [0, 0, 0]
 
+        # The checkpoint node to be reached for reaching final destination
         self.checkpoint = NavSatFix()
+
+        # 
+        self.obs_range_top = LaserScan()
+        self.obs_range_bottom = LaserScan()
 
         # Publisher
         self.pub_checkpoint = rospy.Publisher(
@@ -23,12 +28,24 @@ class PathPlanner():
         # Subscriber
         rospy.Subscriber('/final_setpoint', NavSatFix, final_setpoint_callback)
         rospy.Subscriber('/edrone/gps', NavSatFix, gps_callback)
+        rospy.Subscriber('/edrone/range_finder_top', LaserScan, range_finder_top_callback)
+        rospy.Subscriber('/edrone/range_finder_bottom', LaserScan, range_finder_bottom_callback)
 
     def final_setpoint_callback(self, msg):
         self.destination = [msg.latitude, msg.longitude, msg.altitude]
 
     def gps_callback(self, msg):
         self.current_location = [msg.latitude, msg.longitude, msg.altitude]
+
+    def range_finder_top(self, msg):
+        self.obs_range_top = msg.ranges
+
+    def range_finder_bottom(self, msg):
+        self.obs_range_bottom = msg.ranges
+
+    def data_proccessing(self):
+        '''For Processing the obtained sensor data'''
+        return
 
     def planner(self):
 
