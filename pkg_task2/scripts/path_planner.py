8513ce54a7038a7c253499c539b8d9fdff87cbd8
Rishav Singh <rsh04613@gmail.com>
2020-11-21 17:27:09 +0530

    Testing of Position Controller Bigins

--- pkg_task2/scripts/path_planner.py
+++ pkg_task2/scripts/path_planner.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
+import math
 from sensor_msgs.msg import NavSatFix, LaserScan, Imu
 
 class PathPlanner():
@@ -11,9 +12,11 @@ class PathPlanner():
         # Destination to be reached
         # [latitude, longitude, altitude]
         self.destination = [0, 0, 0]
+        self.destination_xy = [0, 0]
 
         # Present Location of the DroneNote
         self.current_location = [0, 0, 0]
+        self.current_location_xy = [0, 0]
 
         # The checkpoint node to be reached for reaching final destination
         self.checkpoint = NavSatFix()
@@ -23,7 +26,9 @@ class PathPlanner():
         self.obs_range_bottom = []
 
         # self.yaw_error = 0
-        self.diff_range = [0, 0, 0, 0]
+        self.diff_xy = [0, 0]
+        self.distance_xy = 0
+        
         self.movement_in_1D = 0
         self.movement_in_plane = [0, 0]     # [x, y]
         self.obs_closest_range = 2          # closest distance of obstacle (in meters)
@@ -54,19 +59,38 @@ class PathPlanner():
         self.obs_range_bottom = msg.ranges
 
     def lat_to_x(self,input_latitude): return 110692.0702932625 * (input_latitude - 19)
-    def long_to_y(self,input_y): return -105292.0089353767 * (input_longitude - 72)
-    def x_to_lat(self,input_y): return (input_x / 110692.0702932625) + 19
-    def y_to_long(self,input_y): return (input_y / -105292.0089353767) + 72
+    def long_to_y(self,input_longitude): return -105292.0089353767 * (input_longitude - 72)
+
+    def x_to_lat_diff(self,input_x): return (input_x / 110692.0702932625)
+    def y_to_long_diff(self,input_y): return (input_y / -105292.0089353767)
+
+    def calculate_movement_in_plane(self, total_movement):
+        '''This Function will take the drone in straight line towards destination'''
+
+        specific_movement = [0, 0]      # movement in specific direction that is x and y
+
+        # Applying symmetric triangle method
+        specific_movement[0] = total_movement * self.diff_xy[0] / self.distance_xy
+        specific_movement[1] = total_movement * self.diff_xy[1] / self.distance_xy
+        
+        return specific_movement
 
     def obstacle_avoid(self):
-        '''For Processing the obtained sensor data'''
-
+        '''For Processing the obtained sensor data and publishing required 
+        checkpoint for avoiding obstacles'''
+       
         data = self.obs_range_top
         self.movement_in_plane = [0, 0]
-        # self.checkpoint.latitude = self.destination[0]
-        # self.checkpoint.longitude = self.destination[1]
-        # self.checkpoint.altitude = self.destination[2]
 
+        # destination in x and y form
+        self.current_location_xy = [self.lat_to_x(
+            self.destination[1]), self.long_to_y(self.destination[2])]
+        self.destination_xy = [self.lat_to_x(
+            self.current_location[1]), self.long_to_y(self.current_location[2])]
+        self.diff_xy = [self.destination_xy[0] - self.current_location_xy[0],
+                self.destination_xy[1] - self.current_location_xy[1]]
+        self.distance_xy = math.hypot(side[0], side[1])
+        
 
         for obs_distance in data:
             if 12 <= obs_distance <= 25:
@@ -76,7 +100,8 @@ class PathPlanner():
             else:
                 self.movement_in_1D = 2
 
-        self.movement_in_plane = self.calculate_movement_in_plane(self.movement_in_1D)
+        if self.movement_in_1D >= self.distance_xy:
+            self.movement_in_1D = self.distance_xy
 
         for i in range(len(self.obs_range_top)-1):
             if self.obs_range_top[i] <= self.obs_closest_range:
@@ -86,7 +111,13 @@ class PathPlanner():
                 else:
                     self.movement_in_plane[0] = self.movement_in_1D
                     self.movement_in_plane[1] += data[i] - self.obs_closest_range
+        
+            else:
+                self.movement_in_plane = self.calculate_movement_in_plane(self.movement_in_1D)
 
+        self.checkpoint.latitude = self.current_location[0] - self.x_to_lat_diff(self.movement_in_plane[0])
+        self.checkpoint.latitude = self.current_location[1] - self.y_to_long_diff(self.movement_in_plane[1])
+        self.checkpoint.altitude = 25
 
         self.pub_checkpoint.publish(self.checkpoint)
 
