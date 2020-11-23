29ea999e750e3565abe120e722029d8afa5e3b82
Rishav Singh <rsh04613@gmail.com>
2020-11-23 18:18:23 +0530

    Task 2: Submitted

--- pkg_task2/scripts/gripper_client.py
+++ pkg_task2/scripts/gripper_client.py
@@ -7,22 +7,20 @@ import rospy
 
 class Grip():
     def __init__(self):
-        rospy.init_node('gripper_client')#initializing a node
+        rospy.init_node('gripper_client')   # initializing a node
         #necessary variables
         self.gps_position = [0, 0, 0]
         self.final_destination = [0, 0, 0]
         self.container = [0, 0, 0]
-        self.attech_situation = False#check if box is attechable
-        self.attech_constraint = 0#limits the service for atteching request
-        self.reading_constraint = 0#limits the process reletive to the number boxes
-        self.detech_constraint = 0#limits the service for deteching request
-        self.attech_detech_switch = False#determines when to attech box and when to detech the box
+        self.attech_situation = False       # check if box is attechable
+        self.attech_constraint = 0          # limits the service for atteching request
+        self.reading_constraint = 0         # limits the process reletive to the number boxes
+        self.detech_constraint = 0          # limits the service for deteching request
+        self.attech_detech_switch = False   # determines when to attech box and when to detech the box
 
-        rospy.Subscriber('/edrone/gripper_check', String,
-                         self.gripper_check_callback)
+        rospy.Subscriber('/edrone/gripper_check', String, self.gripper_check_callback)
         rospy.Subscriber('/edrone/gps', NavSatFix, self.gps_callback)
-        rospy.Subscriber('/final_setpoint', NavSatFix,
-                         self.final_destination_callback)
+        rospy.Subscriber('/final_setpoint', NavSatFix, self.final_destination_callback)
 
     def final_destination_callback(self, msg):
         '''function will take final destinations from qr_detect.py script'''
@@ -39,13 +37,15 @@ class Grip():
 
     def gripper_client(self, check_condition):
         '''this function will call and wait for the gripper service'''
+
         rospy.wait_for_service('/edrone/activate_gripper')
         carry = rospy.ServiceProxy('/edrone/activate_gripper', Gripper)
         msg_container = carry(check_condition)
-        return msg_container.result#true if box is atteched and visa versa
+        return msg_container.result     # true if box is atteched and visa versa
 
     def grip_check(self):
         '''this function will handle attech and detech service'''
+        
         if(-0.00001517 < (self.final_destination[0]-self.gps_position[0]) < 0.00001517):
             if(-0.15 <= (self.final_destination[2]-self.gps_position[2]) <= 0.15):
                 if(self.reading_constraint % 2 != 0):
--- pkg_task2/scripts/qr_detect.py
+++ pkg_task2/scripts/qr_detect.py
@@ -15,16 +15,20 @@ class image_proc():
 
     # Initialise everything
     def __init__(self):
-        rospy.init_node('qrcode_scan')  # Initialise rosnode
+        rospy.init_node('barcode_scan')  # Initialise rosnode
 
         # This will contain your image frame from camera
         self.img = np.empty([])
+
+        # Subscribing to the camera topic
+        self.image_sub = rospy.Subscriber(
+            "/edrone/camera/image_raw", Image, self.image_callback)
         self.bridge = CvBridge()
 
         # sample time used for defining certain frequency of data input
         self.sample_time = 0.1
 
-        self.switch=True
+        self.box_switch=True
         self.attech_situation='False'
         self.data = [0, 0, 0]
 
@@ -37,9 +41,9 @@ class image_proc():
         rospy.Subscriber('/edrone/gripper_check', String, self.gripper_check_callback)
         #rospy.Subscriber('check_point_flag', Float32, self.detech_msg)
 
+    
     def gripper_check_callback(self, state):
         self.attech_situation = state.data
-
 
     def detech_msg(self, msg):
         self.detech_req = msg.data
@@ -49,42 +53,29 @@ class image_proc():
         try:
             # Converting the image to OpenCV standard image
             self.img = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            cv2.imshow("Image window", self.img)
         except CvBridgeError as e:
-            # print(e)
+            print(e)
             return
 
     def read_qr(self):
-        '''Image QR-Code scanning and publishing algo'''
-        try:
-            barcode = decode(self.img)
-            # used for loop to eleminate the possibility of multiple or null qrcode check
-            for code in barcode:
-                self.data = code.data.decode('utf-8')
-                self.data = list(map(float, self.data.split(',')))
-            # cv2.imshow("show",self.img)
-            # cv2.waitKey(100)
-            print(data)
+        print(type(self.img), int(self.img))
+        # barcode = decode(self.img)
+        print(self.img)
+        # # (rows, cols, channels) = self.img.shape
+        # if cols > 60 and rows > 60:
+        #     cv2.circle(self.img, (50, 50), 10, 255)
 
-            if(self.switch and self.attech_situation=='True' and self.data!=[0,0,0]):
-                self.destination.latitude = self.data[0]
-                self.destination.longitude = self.data[1]
-                self.destination.altitude = self.data[2]
-                #print(self.destination)
-                self.switch=False
-    
-            elif(self.switch):
-                # Publishing the coordinates of box if box is not attached or data is not scanned
-                self.destination.latitude = 19.0007046575 
-                self.destination.longitude = 71.9998955286
-                self.destination.altitude = 22.1599967919
-
-            print(self.destination)
-            self.final_destination.publish(self.destination)
-
-        except ValueError, IndexError:
-            pass
+        cv2.imshow("Image window", self.img)
+        # cv2.imshow(self.img)
+        # for code in barcode:
+        #     print(code.data.decode('utf-8'))
 
 
 if __name__ == '__main__':
     image_proc_obj = image_proc()
+    r = rospy.Rate(1/1)
+    while not rospy.is_shutdown():
+        image_proc_obj.read_qr()
+        r.sleep()
     rospy.spin()
