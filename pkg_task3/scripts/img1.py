811275b0ce1e5d3c4cb2ffe0ae6d4be4d7de1163
Rishav Singh <rsh04613@gmail.com>
2020-12-23 19:35:02 +0530

    img1 modified

--- pkg_task3/scripts/img1.py
+++ pkg_task3/scripts/img1.py
@@ -29,6 +29,7 @@ class marker_detection():
         self.sample_time = 0.1
         self.logo_data=[0,0,0,0]
         self.obs_range_bottom=[0]
+        self.logo_cascade = cv2.CascadeClassifier(os.path.join(os.path.dirname(os.path.realpath(__file__)),'../data/cascade.xml'))
 
         self.image_sub = rospy.Subscriber('/edrone/camera/image_raw', Image, self.image_callback)
         rospy.Subscriber('/edrone/range_finder_bottom', LaserScan, self.range_finder_bottom_callback)
@@ -36,46 +37,32 @@ class marker_detection():
 
         self.marker_error = rospy.Publisher('/marker_error', NavSatFix, queue_size=1)
 
-
-
-
     def range_finder_bottom_callback(self, msg):
         self.obs_range_bottom = msg.ranges
-
 
     def image_callback(self, data):
         ''' Callback function of camera topic'''
         try:
             # Converting the image to OpenCV standard image
-            #print(type(data))
             self.img = self.bridge.imgmsg_to_cv2(data, "bgr8")
             # cv2.imshow("show",self.img)
             #cv2.waitKey(100)
         except CvBridgeError as e:
-            # print(e)
+            print(e)
             return
 
     def detect_marker(self):
         '''Image QR-Code scanning and publishing algo'''
         if(self.img.size>1):
             try:
-                logo_cascade = cv2.CascadeClassifier(os.path.join(os.path.dirname(os.path.realpath(__file__)),'../data/cascade.xml'))
-            #print(self.img.size)
-            #print(type(self.img[1][1][1)
-
-            # img = cv2.imread('test_2.png')  # Source image
-            #self.img=self.img.astype(np.uint8)
+                
                 gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
 
             # image, reject levels level weights.
-                logo = logo_cascade.detectMultiScale(gray, scaleFactor=1.05)
+                logo = self.logo_cascade.detectMultiScale(gray, scaleFactor=1.05)
                 # print(logo[0])
                 if(len(logo)!=0):
-                    # print("detected")
-                    print(logo)
-                    # for i in range(4):
                     if(logo[0][1]>200):
-
                         row_y=row_x=-(200-(2*logo[0][0]+logo[0][2])/2)
                     else:
                         if(logo[0][0]<200):
@@ -84,32 +71,18 @@ class marker_detection():
                         else:
                             row_x=-(200-(2*logo[0][0]+logo[0][2])/2)
                             row_y=200-(2*logo[0][1]+logo[0][3])/2
-
-
-                        
-
-
-                    # row_y=(200-(2*logo[0][1]+logo[0][3])/2)
-
-                        
-                        
                     
                     x=(row_x*self.obs_range_bottom[0])/self.focal_length
                     y=(row_y*self.obs_range_bottom[0])/self.focal_length
-                    # (((self.logo_data[1]+self.logo_data[3])/2)*self.obs_range_bottom[0])/self.focal_length
+                    
                     self.error.latitude=x
                     self.error.longitude=y
-                    print("hello")
-                    # print(self.logo_data)
                     self.marker_error.publish(self.error)
 
-                for (x, y, w, h) in logo:
-
-                    cv2.rectangle(self.img, (x, y), (x + w, y + h), (255, 255, 0), 2)
-                # plt.imshow(cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB))
-                # plt.show()
-                cv2.imshow("show",self.img)
-                cv2.waitKey(100)
+                # for (x, y, w, h) in logo:
+                #     cv2.rectangle(self.img, (x, y), (x + w, y + h), (255, 255, 0), 2)
+                # cv2.imshow("show",self.img)
+                # cv2.waitKey(100)
             except ValueError, IndexError:
                 pass
 
@@ -118,7 +91,6 @@ if __name__ == '__main__':
     marker_detection_obj = marker_detection()
     r = rospy.Rate(1/marker_detection_obj.sample_time)
     while not rospy.is_shutdown():
-
         marker_detection_obj.detect_marker()
         r.sleep()
     rospy.spin()
