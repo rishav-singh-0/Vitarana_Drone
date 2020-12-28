30fa35561903210f7f200f3f7d55cb7d99896b48
379-kash <kashyap379@gmail.com>
2020-12-28 22:57:16 +0530

    Revert "Coordinate Bugs Fixed"
    This reverts commit f9ac5553b46a7ac875aeb392eb67ab08e22c999d.

--- pkg_task4/scripts/position_controller.py
+++ pkg_task4/scripts/position_controller.py
@@ -3,14 +3,17 @@
 # importing required libraries
 
 import rospy
-from vitarana_drone.msg import edrone_cmd
+from vitarana_drone.msg import *
 from pid_tune.msg import PidTune
 from std_msgs.msg import Float32
 from sensor_msgs.msg import NavSatFix
 
+# class for position_controller.py
+
 
 class Command():
     '''Navigating the Drone through commands'''
+    # constructor
 
     def __init__(self):
         rospy.init_node('position_controller')
@@ -21,7 +24,9 @@ class Command():
 
         # initialising desired position
         # [latitude,longitude,altitude]
-        self.destination = [0, 0, 0]
+        self.destination = [[19.0, 72.0, 3],
+                            [19.0000451704, 72.0, 3],
+                            [19.0000451704, 72.0, 0.31]]
         self.next_destination = 0
         # The threshold box can be calculated by using the tolerance of 0.000004517 in latitude, 0.0000047487 in longitude and 0.2m in altitude.
 
@@ -38,25 +43,42 @@ class Command():
         self.output = [0, 0, 0]
 
         self.sample_time = 0.1  # sample time
-
         # equilibrium point for all the angle(roll,pitch,yaw) and throttle
         self.equilibrium_value = 1500
-        
         # object for publishing the rc messages
         self.setpoint_cmd = edrone_cmd()
 
         # Publishing /pitch_error, /roll_error, /throttle_error
-        self.setpoint_pub = rospy.Publisher('/drone_command', edrone_cmd, queue_size=1)
+        self.setpoint_pub = rospy.Publisher(
+            '/drone_command', edrone_cmd, queue_size=1)
         self.pitch_pub = rospy.Publisher('/pitch_error', Float32, queue_size=1)
         self.roll_pub = rospy.Publisher('/roll_error', Float32, queue_size=1)
-        self.throttle_pub = rospy.Publisher('/throttle_error', Float32, queue_size=1)
+        self.throttle_pub = rospy.Publisher(
+            '/throttle_error', Float32, queue_size=1)
 
         # Subscribers
         rospy.Subscriber('/edrone/gps', NavSatFix, self.gps_callback)
+        # rospy.Subscriber('/pid_tuning_altitude',PidTune, self.throttle_set_pid)
+        rospy.Subscriber('/pid_tuning_roll', PidTune, self.roll_set_pid)
+        # rospy.Subscriber('/pid_tuning_pitch', PidTune, self.pitch_set_pid)
 
     def gps_callback(self, msg):
         self.gps_position = [msg.latitude, msg.longitude, msg.altitude]
 
+    def roll_set_pid(self, roll):
+        self.Kp[0] = roll.Kp * 1000
+        self.Ki[0] = roll.Ki * 0.008
+        self.Kd[0] = roll.Kd * 1000*5
+
+    # def pitch_set_pid(self, pitch):
+    #     self.Kp[1] = pitch.Kp * 100
+    #     self.Ki[1] = pitch.Ki * 0.008
+    #     self.Kd[1] = pitch.Kd * 0.3
+
+    # def throttle_set_pid(self, throttle):
+    #     self.Kp[2] = throttle.Kp * 1
+    #     self.Ki[2] = throttle.Ki * 0.25
+    #     self.Kd[2] = throttle.Kd * 1
 
     # this function will convert all rc messages in the range of 1000 to 2000
     def check(self, operator):
@@ -69,19 +91,40 @@ class Command():
         else:
             return operator
 
+    def destination_check(self):
+        ''' function will hendle all desired positions '''
+
+        i = self.next_destination
+        if i == 2:
+            return
+
+        if -0.000004517 <= self.error[0] <= 0.000004517:
+            if -0.0000047487 <= self.error[1] <= 0.0000047487:
+                # here we have taken more accuracy apart from Threshould box
+                if -0.2 <= self.error[2] <= 0.2:
+                    self.next_destination += 1
+                    print("destination reached")
+    # function for implimenting the pid algorithm
+
     def pid(self):
 
         for i in range(3):
-            self.error[i] = self.destination[self.next_destination][i] - self.gps_position[i]
-            self.change[i] = (self.error[i] - self.prev_error[i]) / self.sample_time
+            self.error[i] = self.destination[self.next_destination][i] - \
+                self.gps_position[i]
+            self.change[i] = (
+                self.error[i] - self.prev_error[i]) / self.sample_time
             self.prev_error[i] = self.error[i]
             self.sum[i] = self.sum[i] + self.error[i] * self.sample_time
-            self.output[i] = self.Kp[i] * self.error[i] + self.Kd[i]*self.change[i] + self.Ki[i]*self.sum[i]
+            self.output[i] = self.Kp[i] * self.error[i] + \
+                self.Kd[i]*self.change[i] + self.Ki[i]*self.sum[i]
 
         # figure out the values  for roll,pitch and throttle
-        self.setpoint_cmd.rcRoll = self.check(self.equilibrium_value + self.output[0])
-        self.setpoint_cmd.rcPitch = self.check(self.equilibrium_value + self.output[1])
-        self.setpoint_cmd.rcThrottle = self.check(self.equilibrium_value + self.output[2])
+        self.setpoint_cmd.rcRoll = self.check(
+            self.equilibrium_value + self.output[0])
+        self.setpoint_cmd.rcPitch = self.check(
+            self.equilibrium_value + self.output[1])
+        self.setpoint_cmd.rcThrottle = self.check(
+            self.equilibrium_value + self.output[2])
         self.setpoint_cmd.rcYaw = self.equilibrium_value
 
         # publishing all the values to attitude_controller and for plotting purpose
@@ -98,4 +141,5 @@ if __name__ == '__main__':
     rate = rospy.Rate(1/command.sample_time)  # defining rate
     while not rospy.is_shutdown():
         command.pid()
+        command.destination_check()
         rate.sleep()  # frequency of 100 Hz
